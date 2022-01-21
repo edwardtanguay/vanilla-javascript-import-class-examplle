bbfd03670a6b5e5c0c7f2927c6a1cda60cb636c9
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-21 11:27:15 +0100

    finished

--- js/card_as_class.js
+++ js/card_as_class.js
@@ -3,13 +3,16 @@ export class Card {
 	constructor(elem, title) {
 		this.elem = elem;
 		this.title = title;
+		this.status = 'offline'; // online or offline
 	}
 
 	display() {
 		this.elem.innerHTML += `
-<div class="card">
+<div class="card ${this.status}">
 <h2>${this.title}</h2>
-	Information
+	<p>Information</p>
+	<p>${this.status}</p>
+	<p>${this.status === 'offline' ? 'You cannot access data.' : 'You have access to all data available.'}</p>
 </div>
 `;
 	}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { attachCard, getInfo } from './card_as_function.js';
+import { Card } from './card_as_class.js';
 
 const contentElement = document.querySelector('.content');
 
@@ -6,3 +7,7 @@ contentElement.innerHTML += getInfo();
 for (let x = 1; x <= 3; x++) {
 	attachCard(contentElement, `Card as Function ${x}`);
 }
+
+const card = new Card(contentElement, `Card as Class`);
+card.status = 'online';
+card.display();
